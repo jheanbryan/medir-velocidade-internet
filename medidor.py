9530c589eb73967e1b4eadf88aa317a0c3eb4bc4
Jhean Bryan <jheanbeyan123@gmail.com>
2022-05-17 18:35:23 -0400

    updates

--- medidor.py
+++ medidor.py
@@ -9,14 +9,13 @@ now = datetime.datetime.now()
 date = now.strftime("%d/%m/%Y")
 hour = now.strftime("%H:%M")
 try:
-    download = (test.download()) / 1e+6
-    upload = (test.upload()) / 1e+6
+    download = (test.download()) / 1e+6 #otendo a velocidade de download em mb
+    upload = (test.upload()) / 1e+6 #otendo a velocidade de upload em mb
     try:
         print('DOWNLOAD: {:.2f} Mbps'.format(download))
         print('UPLOAD: {:.2f} Mbps'.format(upload))
         with open('relatorio.txt', 'a') as arquivo:
-            arquivo.write('\n{} - {}    Download:{:.2f}    Upload:{:.2f}'.format(date, hour, download, upload))
-
+            arquivo.write('\n{} - {}    Download:{:.2f} Mbps    Upload:{:.2f} Mbps'.format(date, hour, download, upload))
     except:
         print('[ERRO]: Desconhecido')
 except:
