1f286dbbfb4e5d76f477e84fd13bfaef7a05b501
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-08-23 10:41:30 -0400

    add interface
    adicionada interface simples para realizar a medição da internet

--- interface.py
+++ interface.py
@@ -1,27 +1,52 @@
 import PySimpleGUI as sg
+import speedtest as sp
+import datetime
 
-def second_window():
+def medir_velocidade():
+    test = sp.Speedtest()
+    now = datetime.datetime.now()
+    date = now.strftime("%d/%m/%Y")
+    hour = now.strftime("%H:%M")
+
+    try:
+        print('Medindo...')
+        download = (test.download()) / 1e+6
+        upload = (test.upload()) / 1e+6
+
+        velocidade_download = 'DOWNLOAD: {:.2f} Mbps'.format(download)
+        velocidade_upload = 'UPLOAD: {:.2f} Mbps'.format(upload)
+
+        with open('relatorio.txt', 'a') as arquivo:
+            arquivo.write('\n{} - {}    Download:{:.2f} Mbps    Upload:{:.2f} Mbps'.format(date, hour, download, upload))
+
+        return velocidade_download, velocidade_upload
+
+    except:
+        print('[ERRO]: ao conectar ao servidor')
+
+def second_window(download, upload):
     layout = [ 
-        [sg.Text('A velocidade da sua internet é tal')] 
+        [sg.Text(download)],
+        [sg.Text(upload)],
+        [sg.Text('Foi salvo um relatório com o nome relatorio.txt')],
+        [sg.Button('Ok')]
     ]
 
     window_res = sg.Window('Medidor de Velocidade', layout)
 
     event, values = window_res.read()
-    if event == sg.WIN_CLOSED or event == 'Cancelar':
+    if event == sg.WIN_CLOSED or event == 'Ok':
         window_res.close()
-
 
 def initial_window():
     sg.theme('Dark Grey 13') 
 
     layout = [ 
-                [sg.Text('Clique em Iniciar o teste')],
+                [sg.Text('Clique em Iniciar para começar o teste', justification='center')],
                 [sg.Button('Iniciar'), sg.Button('Cancelar')] 
             ]
 
-    window = sg.Window('Medidor de Velocidade', layout)
-
+    window = sg.Window('Medidor de Velocidade', layout, size=(300, 70))
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancelar':
@@ -30,7 +55,9 @@ def initial_window():
         else:
             #calcular a velocidade da internte
             print('vou calcular')
-            second_window()
             window.close()
 
 initial_window()
+velocidade_download, velocidade_upload = medir_velocidade()
+
+second_window(velocidade_download, velocidade_upload)
--- medidor.py
+++ medidor.py
@@ -4,19 +4,23 @@
 import speedtest as sp
 import datetime
 
-test = sp.Speedtest()
-now = datetime.datetime.now()
-date = now.strftime("%d/%m/%Y")
-hour = now.strftime("%H:%M")
-try:
-    download = (test.download()) / 1e+6 #otendo a velocidade de download em mb
-    upload = (test.upload()) / 1e+6 #otendo a velocidade de upload em mb
+def medir_velocidade():
+    test = sp.Speedtest()
+    now = datetime.datetime.now()
+    date = now.strftime("%d/%m/%Y")
+    hour = now.strftime("%H:%M")
+
     try:
-        print('DOWNLOAD: {:.2f} Mbps'.format(download))
-        print('UPLOAD: {:.2f} Mbps'.format(upload))
-        with open('relatorio.txt', 'a') as arquivo:
-            arquivo.write('\n{} - {}    Download:{:.2f} Mbps    Upload:{:.2f} Mbps'.format(date, hour, download, upload))
+        print('Medindo...')
+        download = (test.download()) / 1e+6
+        upload = (test.upload()) / 1e+6
+        try:
+            velocidadeDownload = 'DOWNLOAD: {:.2f} Mbps'.format(download)
+            velocidadeUpload = 'UPLOAD: {:.2f} Mbps'.format(upload)
+
+            with open('relatorio.txt', 'a') as arquivo:
+                arquivo.write('\n{} - {}    Download:{:.2f} Mbps    Upload:{:.2f} Mbps'.format(date, hour, download, upload))
+        except:
+            print('[ERRO]: Desconhecido')
     except:
-        print('[ERRO]: Desconhecido')
-except:
-    print('[ERRO]: ao conectar ao servidor')
+        print('[ERRO]: ao conectar ao servidor')
